3918ffef2ed9a7161c66c4f6d648f3f7bb91c5bd
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-03-08 19:46:07 -0700

    Added index position explanation

--- arrays.rb
+++ arrays.rb
@@ -10,3 +10,5 @@ deposit_paid.shift
 working = [true, true, false, true, true]
 # The unshift method will add the new item, false, to the beginning of the array.
 working.unshift(false)
+
+# In the ticket_prices array, the index position of the integer 40 is 2, and the value of the element in index position 4 is 60. In the deposit_paid array, there are 4 elements and the value of the element in index position 2 is 1400.00.
