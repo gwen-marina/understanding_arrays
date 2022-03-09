75768c57582169b3228237482317c9b0e307815d
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-03-08 19:15:25 -0700

    Added arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+students = ["Gwendolyn", "Oliver", "Nick", "Cara"]
+ticket_prices = [25, 30, 40, 50, 60]
+deposit_paid = [1200.00, 1200.00, 1400.00, 2000.00]
+working = [true, true, false, true, true]
