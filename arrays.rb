9e3a3ba2a653afe588695eaff684f1a11b849140
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-03-08 19:26:07 -0700

    Added 4 methods and ruby comment

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,12 @@
 students = ["Gwendolyn", "Oliver", "Nick", "Cara"]
+# The pop method will remove the last element of the array, so Cara will be gone from the array.
+students.pop
 ticket_prices = [25, 30, 40, 50, 60]
+# The push method will add the element 70 to the end of the array.
+ticket_prices.push(70)
 deposit_paid = [1200.00, 1200.00, 1400.00, 2000.00]
+# The shift method removes the first element in the array so 1200.00 will be removed.
+deposit_paid.shift
 working = [true, true, false, true, true]
+# The unshift method will add the new item, false, to the beginning of the array.
+working.unshift(false)
